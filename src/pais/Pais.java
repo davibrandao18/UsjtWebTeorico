aab3b81ec1d6ad755aa72792deb3869d10340b3c
Maicon Souza <maiconsouzapa@gmail.com>
2020-03-22 17:59:50 -0300

    Classe pais, atributos, construtores, getters e setters

---
+++ src/pais/Pais.java
@@ -0,0 +1,53 @@
+package pais;
+
+public class Pais {
+	private int id;
+	private String nome;
+	private long populacao;
+	private double area;
+	
+	public Pais() {}
+	
+	public Pais(int id, String nome, long populacao, double area) {
+		this.id = id;
+		this.nome = nome;
+		this.populacao = populacao;
+		this.area = area;
+	}
+
+	// Getters
+	public int getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public long getPopulacao() {
+		return populacao;
+	}
+
+	public double getArea() {
+		return area;
+	}
+
+	// Setters
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setPopulacao(long populacao) {
+		this.populacao = populacao;
+	}
+
+	public void setArea(double area) {
+		this.area = area;
+	}
+	
+	
+}
